eded84e8a44209197367963fe78be9bc74d83f02
Prateek Mehta <136672247+Prateekmehta09@users.noreply.github.com>
2024-07-20 20:29:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Magical Number/magical-number.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[] arr = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            int res = obj.findMagicalNumber(N, arr);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int findMagicalNumber(int N, int[] arr) {
+        // code here
+        int s=0;
+        int e=N-1;
+        int ans=-1;
+        
+        while(s<=e)
+        {
+            int m=s+(e-s)/2;
+            
+            if(arr[m]>=m){
+                if(arr[m]==m)
+                    ans=m;
+                e=m-1;
+                
+            }
+            else{
+                s=m+1;
+            }
+            
+        }
+        return ans;
+        }
+    
+}
+        
