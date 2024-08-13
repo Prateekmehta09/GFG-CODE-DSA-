cb72106dd77c5366dcef4c9c73aaa64a46859f9c
Prateek Mehta <136672247+Prateekmehta09@users.noreply.github.com>
2024-08-13 20:23:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Square root of a number/square-root-of-a-number.java
+++ Difficulty: Easy/Square root of a number/square-root-of-a-number.java
@@ -1,20 +1,17 @@
 //{ Driver Code Starts
 import java.util.Scanner;
 
-class SquartRoot
-{
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		while(t>0)
-		{
-			long a = sc.nextInt();
-			Solution obj = new Solution();
-			System.out.println(obj.floorSqrt(a));
-		t--;
-		}
-	}
+class SquartRoot {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            long a = sc.nextInt();
+            Solution obj = new Solution();
+            System.out.println(obj.floorSqrt(a));
+            t--;
+        }
+    }
 }
 // } Driver Code Ends
 
@@ -24,30 +21,10 @@ this function*/
 
 // Function to find square root
 // x: element to find square root
-class Solution
-{
-     long floorSqrt(long x)
-	 {
-		// Your code here
-		
-		// O(n).... 
-// 		long  ans=1;
-// 		for(long i=1;i<=x;i++)
-// 		{
-// 		    if(i*i<=x)
-// 		    {
-// 		        ans=i;
-// 		    }
-// 		    else{
-// 		        break;
-// 		    }
-// 		}
-// 		return ans;
-//......................................................................
-
-  // O(log n)
-   
-   long low=1;
+class Solution {
+    long floorSqrt(long x) {
+        // Your code here
+    long low=1;
    long high=x;
    long ans=0;
    long mid=low+(high-low)/2;
@@ -65,6 +42,5 @@ class Solution
        mid=low+(high-low)/2;
    }
    return ans;
-		
-	 }
+    }
 }
