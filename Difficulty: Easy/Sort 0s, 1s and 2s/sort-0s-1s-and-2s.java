7b4f558b44779127423dec801336274f01ae41da
Prateek Mehta <136672247+Prateekmehta09@users.noreply.github.com>
2024-12-05 12:39:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -11,11 +11,9 @@ class GFG {
         while (t-- > 0) {
             String input = br.readLine();
             String[] inputArray = input.split("\\s+");
-            ArrayList<Integer> a = new ArrayList<>();
+            int a[] = new int[inputArray.length];
 
-            for (String s : inputArray) {
-                a.add(Integer.parseInt(s));
-            }
+            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(inputArray[i]);
 
             Solution ob = new Solution();
             ob.sort012(a);
@@ -24,26 +22,58 @@ class GFG {
                 System.out.print(num + " ");
             }
             System.out.println();
+            System.out.println("~");
         }
     }
 }
+
 
 // } Driver Code Ends
-
-
 class Solution {
     // Function to sort an array of 0s, 1s, and 2s
-    public void sort012(ArrayList<Integer> arr) {
+    public void sort012(int[] arr) {
         // code here
-        int count0=0,count1=0,count2=0;
-        for(int i=0;i<arr.size();i++)
+        int n=arr.length;
+        int start=0;
+        int mid=0;
+        int end=arr.length-1;
+        
+        while(mid<=end)
         {
-            if(arr.get(i)==0)count0++;
-            else if(arr.get(i)==1)count1++;
-            else count2++;
+            if(arr[mid]==0)
+            {
+               if (start != mid) {
+                    int temp = arr[start];
+                    arr[start] = arr[mid];
+                    arr[mid] = temp;
+                   
+               }
+                    
+                start++;
+                mid++;
+                
+            }
+            else if(arr[mid]==1)
+            {
+                mid++;
+            }
+            else{
+                 if (mid != end) {
+                    int temp = arr[mid];
+                    arr[mid] = arr[end];
+                    arr[end] = temp;
+                }
+                end--;
+            }
         }
-        for(int i=0;i<count0;i++)arr.set(i,0);
-        for(int i=count0;i<count0+count1;i++)arr.set(i,1);
-        for(int i=count1+count0;i<arr.size();i++)arr.set(i,2);
     }
+    // public void swap(int a,int b)
+    // {
+    //     int temp=a;
+    //     a=b;
+    //     b=temp;
+    // }
 }
+
+//{ Driver Code Starts.
+// } Driver Code Ends
