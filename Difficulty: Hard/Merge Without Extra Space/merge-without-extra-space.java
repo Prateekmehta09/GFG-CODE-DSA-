99229dc7dbe6c138e64c3748d0631cc1a9125f6a
Prateek Mehta <136672247+Prateekmehta09@users.noreply.github.com>
2024-12-11 23:54:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Merge Without Extra Space/merge-without-extra-space.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+
+        while (t-- > 0) {
+            String arr1[] = br.readLine().trim().split(" ");
+            String arr2[] = br.readLine().trim().split(" ");
+
+            int a[] = new int[arr1.length];
+            int b[] = new int[arr2.length];
+
+            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(arr1[i]);
+            for (int i = 0; i < b.length; i++) b[i] = Integer.parseInt(arr2[i]);
+
+            Solution ob = new Solution();
+            ob.mergeArrays(a, b);
+
+            StringBuffer str = new StringBuffer();
+            for (int i = 0; i < a.length; i++) {
+                str.append(a[i] + " ");
+            }
+            System.out.println(str);
+            str = new StringBuffer();
+            for (int i = 0; i < b.length; i++) {
+                str.append(b[i] + " ");
+            }
+            System.out.println(str);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to merge the arrays.
+    public void mergeArrays(int arr1[], int arr2[]) {
+        // code here
+        int n=arr1.length;
+        int m=arr2.length;
+        
+        int i=n-1,j=0;
+           while(i>=0 && j<m)
+           {
+               if(arr1[i]>=arr2[j])
+               {
+                     int  temp=arr1[i];
+                     arr1[i]=arr2[j];
+                     arr2[j]=temp;
+               }
+               else {break;}
+               i--;
+               j++;
+           }
+          Arrays.sort(arr1);
+          Arrays.sort(arr2);
+    }
+}
